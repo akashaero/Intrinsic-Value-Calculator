b4f6d6e592e9d89f9cb09c8065bbd0d2a6856c33
Akash Patel <patel_akash1996@yahoo.com>
2024-03-28 21:09:38 -0400

    Some change for incoming data

--- provider.py
+++ provider.py
@@ -60,7 +60,7 @@ def get_info(ticker):
   starting_rev    = float(income.loc['Total Revenue'][0]) if not np.isnan(income.loc['Total Revenue'][0]) else '-'
   FCF_Margin      = round(100*starting_fcf / starting_rev, 2) if not starting_fcf == '-' or not starting_rev == '-' else '-'
   current_price   = round(stock_info['previousClose'], 2)
-  total_shares    = income.loc['Basic Average Shares'][0]
+  total_shares    = stock_info['sharesOutstanding'] if 'sharesOutstanding' in stock_info else income.loc['Basic Average Shares'][0]
   starting_rev    = income.loc['Total Revenue'][0]
   rev_growth      = get_rates(income.loc['Total Revenue'])
   buybacks        = get_rates(income.loc['Diluted Average Shares'])
